7f43f5a87da2178c01bb5b21678cf2ff5c1e898e
MobasherAlam <190139.cse@student.just.edu.bd>
2023-12-22 22:44:19 +0600

    first commit

---
+++ src/components/NavBar/NavBar.jsx
@@ -0,0 +1,39 @@
+import { useState } from "react";
+import Link from "../Link/Link";
+import { LuMenu } from "react-icons/lu";
+import { FaRegWindowClose } from "react-icons/fa";
+
+const NavBar = () => {
+    const [open, setOpen] = useState(false);
+
+    const routes = [
+        { id: 1, path: '/', name: 'Home' },
+        { id: 2, path: '/about', name: 'About' },
+        { id: 3, path: '/services', name: 'Services' },
+        { id: 4, path: '/contact', name: 'Contact' },
+        { id: 5, path: '/*', name: 'NotFound' }
+    ];
+
+    return (
+        <nav>
+            <div className="md:hidden text-xl " onClick={() => setOpen(!open)}>
+                {
+                    open === true ?
+                    <FaRegWindowClose/>
+                    :  <LuMenu/>
+                }  
+            </div>
+
+            <ul className={`md:flex absolute duration-1000 md:static
+                ${open ? 'top-16' : 'hidden'}
+            `}>
+                {
+                    routes.map(route => <Link key={route.id} route={route}></Link>)
+                }
+            </ul>
+
+        </nav>
+    );
+};
+
+export default NavBar;
